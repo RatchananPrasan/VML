446d4f2413ac2ef11723980e831aebb4d949a38a
tigerSI <sirapop.tiger@gmail.com>
2018-04-18 20:34:29 +0700

    resize the imagedata

--- visualization/views.py
+++ visualization/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from visualization_core.visualization_controller import VisualizationController
 
+from visualization_core.mnist import sample
 def index(request):
+    
     context = {}
 
     controller = VisualizationController()
@@ -11,5 +13,12 @@ def index(request):
 
     context["data"] = information["data"]
     context["dotstring"] = information["dotstring"]
+    size, input_data = sample.input_sample_1d()
+
+    context["size"] = size
+    context["content"] = input_data
+
+
+    
 
     return render(request, 'visualization/index.html', context)
